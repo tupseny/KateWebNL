d08603ca2af7eccde679a67b72720ef6458e1635
Martin <marsik98@gmail.com>
2018-06-18 20:59:11 +0300

    First commit

---
+++ src/main/java/com/tupseny/kateweb/KatewebApplication.java
@@ -0,0 +1,12 @@
+package com.tupseny.kateweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatewebApplication.class, args);
+    }
+}
---
+++ src/main/java/com/tupseny/kateweb/controllers/MainController.java
@@ -0,0 +1,44 @@
+package com.tupseny.kateweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home_def(){
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/access-denied")
+    public String error403() {
+        return "/errors/403";
+    }
+
+}
